837f0fed0b1e1614f6ae29c445c1fbcba4def6f5
thanhoanganhtuan <tuan.thanhoanganh@gmail.com>
2023-12-30 21:52:33 +0700

    test

--- admin/controllers/index.js
+++ admin/controllers/index.js
@@ -9,79 +9,74 @@ const type = document.querySelector("#type");
 
 const openAddModalButton = document.querySelector("#openAddModal");
 openAddModalButton.addEventListener("click", () => {
-   renderAddModal();
+    renderAddModal();
 
-   const closeModalButton = document.querySelector("#closeModal");
-   const addPhoneButton = document.querySelector("#addPhone");
-   addPhoneButton.addEventListener("click", () => {
-      if (validation()) {
-         // addProduct();
-         closeModalButton.click();
-         resetForm();
-      }
-   });
+    const closeModalButton = document.querySelector("#closeModal");
+    const addPhoneButton = document.querySelector("#addPhone");
+    addPhoneButton.addEventListener("click", () => {
+        if (validation()) {
+            // addProduct();
+            closeModalButton.click();
+            resetForm();
+        }
+    });
 });
 
 function validation() {
-   const listInputElement = document.querySelectorAll(
-      ".modal-body input"
-   );
-   const validate = Array.from(listInputElement).map(
-      (element, index) => {
-         let isCheck = false;
-         element.addEventListener("input", () => {
+    const listInputElement = document.querySelectorAll(".modal-body input");
+    const validate = Array.from(listInputElement).map((element, index) => {
+        let isCheck = false;
+        element.addEventListener("input", () => {
             setNormal(element);
-         });
+        });
 
-         if (element.value.trim() === "") {
+        if (element.value.trim() === "") {
             setError(element, "Vui lòng nhập trường này");
             isCheck = false;
-         } else {
+        } else {
             setSuccess(element);
             isCheck = true;
-         }
+        }
 
-         if (index === 1) {
+        if (index === 1) {
             const regex = /^[0-9]+$/;
             if (regex.test(element.value)) {
-               setSuccess(element);
-               isCheck = true;
+                setSuccess(element);
+                isCheck = true;
             } else if (element.value.trim() !== "") {
-               setError(element, "Trường này phải là số");
-               isCheck = false;
+                setError(element, "Trường này phải là số");
+                isCheck = false;
             }
-         }
+        }
 
-         if (index === 5) {
+        if (index === 5) {
             const regex =
-               /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+                /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
             if (regex.test(element.value)) {
-               setSuccess(element);
-               isCheck = true;
+                setSuccess(element);
+                isCheck = true;
             } else if (element.value.trim() !== "") {
-               setError(element, "Trường này phải là link");
-               isCheck = false;
+                setError(element, "Trường này phải là link");
+                isCheck = false;
             }
-         }
-         return isCheck;
-      }
-   );
-   let isCheck = false;
-   const selectElement = document.querySelector("#type");
-   selectElement.addEventListener("change", () => {
-      setNormal(selectElement);
-   });
+        }
+        return isCheck;
+    });
+    let isCheck = false;
+    const selectElement = document.querySelector("#type");
+    selectElement.addEventListener("change", () => {
+        setNormal(selectElement);
+    });
 
-   if (selectElement.value === "") {
-      isCheck = false;
-      setError(selectElement, "Vui lòng chọn thương hiệu");
-   } else {
-      isCheck = true;
-      setSuccess(selectElement);
-   }
+    if (selectElement.value === "") {
+        isCheck = false;
+        setError(selectElement, "Vui lòng chọn thương hiệu");
+    } else {
+        isCheck = true;
+        setSuccess(selectElement);
+    }
 
-   return (
-      validate.reduce((total, current) => total && current, true) &&
-      isCheck
-   );
+    return (
+        validate.reduce((total, current) => total && current, true) && isCheck
+    );
 }
--- customer/models/cartItem.js
+++ customer/models/cartItem.js
@@ -1,8 +1,7 @@
-
- function CartItem(_id, _name, _price, _image, _quality) {
+function CartItem(_id, _name, _price, _image, _quality) {
     this._id = _id;
     this._name = _name;
     this._price = _price;
     this._image = _image;
     this._quality = _quality;
- }
+}
